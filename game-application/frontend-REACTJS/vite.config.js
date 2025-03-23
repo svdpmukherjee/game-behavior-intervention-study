0deff03650eeb7aa62178942e2e3ede6826dea32
svdpmukherjee <100602182+svdpmukherjee@users.noreply.github.com>
2025-03-23 19:28:47 +0100

    vite.config.js updated with local & production URLs

--- game-application/frontend-REACTJS/vite.config.js
+++ game-application/frontend-REACTJS/vite.config.js
@@ -1,21 +1,3 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-// import tailwindcss from "@tailwindcss/vite";
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react(), tailwindcss()],
-//   server: {
-//     proxy: {
-//       "/api": {
-//         // target: "https://anagram-game-cheating-detection.onrender.com",
-//         target: "http://localhost:8000",
-//         changeOrigin: true,
-//       },
-//     },
-//   },
-// });
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";

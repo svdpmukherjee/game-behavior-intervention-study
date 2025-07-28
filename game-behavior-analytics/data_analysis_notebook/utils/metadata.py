e0ec10f438336efb7d4aca1149ef1622950443c2
svdpmukherjee <100602182+svdpmukherjee@users.noreply.github.com>
2025-07-28 03:22:55 +0200

    new code files

---
+++ game-behavior-analytics/data_analysis_notebook/utils/metadata.py
@@ -0,0 +1,31 @@
+# -----------------------------------------------------------------------------
+# Theory Metadata: Mapping Concepts to Theories
+# -----------------------------------------------------------------------------
+
+theory_order = [
+    'autonomy', 'competence', 'relatedness',  # Self-Determination
+    'self_concept', 'cognitive_inconsistency', 'dissonance_arousal', 'dissonance_reduction',  # Cognitive Dissonance
+    'performance_accomplishments', 'vicarious_experience', 'verbal_persuasion', 'emotional_arousal',  # Self-Efficacy
+    'descriptive_norms', 'injunctive_norms', 'social_sanctions', 'reference_group_identification'  # Social Norms
+]
+
+theory_map = {
+    'autonomy': 'Self-Determination Theory',
+    'competence': 'Self-Determination Theory',
+    'relatedness': 'Self-Determination Theory',
+    
+    'self_concept': 'Cognitive Dissonance Theory',
+    'cognitive_inconsistency': 'Cognitive Dissonance Theory',
+    'dissonance_arousal': 'Cognitive Dissonance Theory',
+    'dissonance_reduction': 'Cognitive Dissonance Theory',
+    
+    'performance_accomplishments': 'Self-Efficacy Theory',
+    'vicarious_experience': 'Self-Efficacy Theory',
+    'verbal_persuasion': 'Self-Efficacy Theory',
+    'emotional_arousal': 'Self-Efficacy Theory',
+    
+    'descriptive_norms': 'Social Norms Theory',
+    'injunctive_norms': 'Social Norms Theory',
+    'social_sanctions': 'Social Norms Theory',
+    'reference_group_identification': 'Social Norms Theory'
+}
